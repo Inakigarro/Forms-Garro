b3118943c0c0204dedbd2529744984651e5258ea
inakigarro <inakigarro@gmail.com>
2023-04-05 19:33:04 -0300

    feat: create form using reactive forms in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFromComponent } from './forms/reactive-from/reactive-from.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+const materialModules = [MatFormFieldModule, MatInputModule];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ReactiveFromComponent],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    materialModules,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/forms/reactive-from/reactive-from.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-from',
+  templateUrl: './reactive-from.component.html',
+  styleUrls: ['./reactive-from.component.scss'],
+})
+export class ReactiveFromComponent {
+  form: FormGroup;
+  public firstNameControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(70),
+  ]);
+  public lastNameControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(70),
+  ]);
+  public emailControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+    Validators.maxLength(150),
+  ]);
+  public countryControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(70),
+  ]);
+  public cityControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(150),
+  ]);
+  public addressControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(150),
+  ]);
+  public phoneControl = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(20),
+  ]);
+
+  constructor(public formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      firstName: this.firstNameControl,
+      lastName: this.lastNameControl,
+      email: this.emailControl,
+      country: this.countryControl,
+      city: this.cityControl,
+      address: this.addressControl,
+      phone: this.phoneControl,
+    });
+  }
+
+  public onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      this.form.reset();
+    } else {
+      console.error('Esto no funciona');
+    }
+  }
+}
